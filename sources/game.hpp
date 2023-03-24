be3e340e4ea17671805031b8d7d5de2aabe05f18
roey-ubuntu <roey.fenigold@gmail.com>
2023-03-24 17:04:50 +0300

    update game.hpp

--- sources/game.hpp
+++ sources/game.hpp
@@ -1,8 +1,45 @@
-//
-// Created by roey-ubuntu on 3/22/23.
-//
+#include <iostream>
+#include "card.hpp"
+#include "player.hpp"
 
-#ifndef CARDWAR_A_GAME_HPP
-#define CARDWAR_A_GAME_HPP
+using namespace std;
 
-#endif //CARDWAR_A_GAME_HPP
+class Game {
+public:
+    Player p1, p2;
+    Card deck[52];
+
+    Game(Player _p1, Player _p2);
+
+    ~Game();
+
+    void playTurn();
+
+/**
+* @brief Print the last turn stats.
+*/
+    void printLastTurn();
+
+/**
+* @brief Plays the game until the end.
+*/
+    void playAll();
+
+/**
+* @brief Print the name of the winning player.
+*/
+    void printWiner();
+
+/**
+* @brief Print all the turns played one line per turn
+     * (same format as game.printLastTurn()).
+*/
+    void printLog();
+
+/**
+* @brief For each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand.
+     * (draw within a draw counts as 2 draws. ).
+*/
+    void printStats();
+
+};
