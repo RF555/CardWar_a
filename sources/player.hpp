6d86644693537929a207dcf48c72fa514b1a545a
roey-ubuntu <roey.fenigold@gmail.com>
2023-03-24 17:04:42 +0300

    update player.hpp

--- sources/player.hpp
+++ sources/player.hpp
@@ -1,8 +1,26 @@
-//
-// Created by roey-ubuntu on 3/22/23.
-//
+#include <iostream>
+#include "card.hpp"
 
-#ifndef CARDWAR_A_PLAYER_HPP
-#define CARDWAR_A_PLAYER_HPP
+using namespace std;
 
-#endif //CARDWAR_A_PLAYER_HPP
+class Player {
+public:
+    string name;
+    Card deck
+
+    Player(string _name);
+
+    ~Player();
+
+/**
+* @brief Print the amount of cards left.
+ * Should be 21 but can be less if a draw was played.
+*/
+    void stacksize();
+
+/**
+ * @brief Print the amount of cards this player has won.
+ */
+    void cardesTaken();
+
+};
